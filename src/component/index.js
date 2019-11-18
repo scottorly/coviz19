52fdc3389ad7b7e449c4a2fbb7f446097d813a6f
Scott Orlyck <scott.orlyck+orly@gmail.com>
2019-11-18 00:32:17 -0800

    name and remove async

--- src/component/index.js
+++ src/component/index.js
@@ -5,7 +5,7 @@ const TextInput = ({ attributes: { name, className }}) => (
     <div className={className} >
         <label>{name}</label>
         <input id={styles.textInput} type='text'/>
-        <button eventListener={['click', async e => {
+        <button eventListener={['click', e => {
             const message = select(`#${styles.textInput}`).node().value
             window.alert(message)
         }]}>{`${name}`}</button>
