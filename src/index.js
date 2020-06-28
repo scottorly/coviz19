1df5ff63ef55e0fad2c80517fd816e0976ec5e17
ScottORLY <scott.orlyck+orly@gmail.com>
2020-06-28 10:57:11 -0700

    maybe we wont use mp browser

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@
 import styles from './styles.css'
 import ConfirmedCases from './cases'
 import Controls from './controls'
-import mixpanel from 'mixpanel-browser'
+
+mixpanel.track("Page View")
 
 document.body.appendChild(
     <div id={styles.app}>
@@ -28,7 +29,3 @@ document.body.appendChild(
         </div>
     </div>
 )
-
-mixpanel.init('d5a4311d045c79b449719cd17223e378')
-mixpanel.track("Page View")
-window.mixpanel = mixpanel
