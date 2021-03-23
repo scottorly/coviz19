e01bf36bbf0f97cb05c90d817bf6afa21cc96949
ScottORLY <scott.orlyck+orly@gmail.com>
2021-03-23 13:18:36 -0700

    remove webpack

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@
 import styles from './styles.module.css'
 import Choropleth from './choropleth'
 import Footer from './footer'
-console.log('hello world')
+
 document.body.appendChild(
     <div id={styles.app}>
         <Choropleth />
